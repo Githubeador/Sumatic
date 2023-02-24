07d8e730cd7b93c87e681c67c1751525c520fdcf
Githubeador <contacta.duarte@gmail.com>
2023-02-24 23:35:37 +0100

    Testeo 2.0

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('Presione uno de los botones:')],
-          [sg.Button('Botón 1', key='boton1'), sg.Button('Botón 2', key='boton2')]]
+sg.theme('LightBlue')
 
-window = sg.Window('Ejemplo de botones', layout)
+layout = [
+    [sg.Image("fondo montañas.jpg"],
+    [sg.Text('Hola, mundo!')]
+]
+
+window = sg.Window('Mi ventana', layout)
 
 while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'boton1':
-        print('Ha presionado el botón 1')
-    elif event == 'boton2':
-        print('Ha presionado el botón 2')
 
 window.close()
-#
+
 
--- test.py
+++ test.py
@@ -2,18 +2,17 @@ import PySimpleGUI as sg
 
 sg.theme("DarkAmber")
 
-layout = [[sg.Button("PULSA AQUÍ")]]
+layout1 = [[sg.Button("PULSA AQUÍ")]]
 
 windows = sg.Window("Sumatic", layout, size=(1920,1080))
 
+layout2 =
 while True:
     event, values = windows.read()
     if event == sg.WIN_CLOSED:
         break
-    if event == "PULSA AQUÍ":
-        sg.Popup("HOLA")
 
 windows.close()
-#
+
 
 
