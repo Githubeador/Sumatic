3ec3d16b3ac7edfac5293c3d37481f30c51f3993
37632 <contacta.duarte@gmail.com>
2023-02-28 13:48:52 +0100

    1RAS PRUEBAS DSD EL COLE

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import PySimpleGUI as sg
 
-layout = [[sg.Text('Presione uno de los botones:')],
-          [sg.Button('Botón 1', key='boton1'), sg.Button('Botón 2', key='boton2')]]
+sg.theme("DarkAmber")
 
-window = sg.Window('Ejemplo de botones', layout)
+layout = [[sg.Text("PULSA AQUÍ")]]
+
+windows = sg.Window("Sumatic", layout, size=(1920,1080))
 
 while True:
-    event, values = window.read()
+    event, values = windows.read()
     if event == sg.WIN_CLOSED:
         break
-    elif event == 'boton1':
-        print('Ha presionado el botón 1')
-    elif event == 'boton2':
-        print('Ha presionado el botón 2')
+    if event == "PULSA AQUÍ":
+        sg.Popup("HOLA")
 
-window.close()
+windows.close()
 #
 
